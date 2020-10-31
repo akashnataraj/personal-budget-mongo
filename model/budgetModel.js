cfa972f436960aca3efecbf8bccb058b8208f950
Akash Nataraj <akash.nataraj@gmail.com>
2020-10-30 21:51:22 -0400

    color fix

--- model/budgetModel.js
+++ model/budgetModel.js
@@ -1,21 +1,27 @@
-var mongoose = require('mongoose');
-var budgetSchema = new mongoose.Schema({
+var mongoose = require("mongoose");
+var budgetSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        trim: true,
-        required: true,
-        uppercase: true
+      type: String,
+      trim: true,
+      required: true,
+      uppercase: true,
     },
     budget: {
-        type: Number,
-        required: true,
-        unique: false
+      type: Number,
+      required: true,
+      unique: false,
     },
     color: {
-        type: String,
-        required: true,
-        unique: false
-    }
-}, {collection: 'budget'});
+      type: String,
+      required: true,
+      //1:# + 6:Hexcode
+      minlength: 7,
+      maxlength: 7,
+      unique: false,
+    },
+  },
+  { collection: "budget" }
+);
 
-module.exports = mongoose.model('Budget', budgetSchema);    
+module.exports = mongoose.model("Budget", budgetSchema);
